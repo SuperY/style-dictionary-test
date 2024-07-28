9e7d4d09590bf409240c94ac9c0de10ced36af30
SuperY <felix_dev@163.com>
2024-07-29 00:31:18 +0900

    Update transformTokens.js

--- transformTokens.js
+++ transformTokens.js
@@ -3,27 +3,27 @@ const deepMerge = require("deepmerge");
 // const webConfig = require('./src/web/index.js')
 // const androidConfig = require("./src/android/index.js");
 
-StyleDictionary.registerTransform({
-  name: 'size/px',
-  type: 'value',
-  matcher: token => {
-    return (token.unit === 'pixel' || token.type === 'dimension') && token.value !== 0
-  },
-  transformer: token => {
-    return `${token.value}px`
-  }
-})
+// StyleDictionary.registerTransform({
+//   name: 'size/px',
+//   type: 'value',
+//   matcher: token => {
+//     return (token.unit === 'pixel' || token.type === 'dimension') && token.value !== 0
+//   },
+//   transformer: token => {
+//     return `${token.value}px`
+//   }
+// })
 
-StyleDictionary.registerTransform({
-  name: 'size/percent',
-  type: 'value',
-  matcher: token => {
-    return token.unit === 'percent' && token.value !== 0
-  },
-  transformer: token => {
-    return `${token.value}%`
-  }
-})
+// StyleDictionary.registerTransform({
+//   name: 'size/percent',
+//   type: 'value',
+//   matcher: token => {
+//     return token.unit === 'percent' && token.value !== 0
+//   },
+//   transformer: token => {
+//     return `${token.value}%`
+//   }
+// })
 
 StyleDictionary.registerFilter({
   name: 'validToken',
@@ -33,11 +33,6 @@ StyleDictionary.registerFilter({
       "string",
       "number",
       "color",
-      "custom-spacing",
-      "custom-gradient",
-      "custom-fontStyle",
-      "custom-radius",
-      "custom-shadow",
     ].includes(token.type);
   }
 })
